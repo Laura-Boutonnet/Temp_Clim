d8ddb2e467efa50204c64f297e1d4caf26705e60
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-07-19 15:21:00 +0200

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -8,10 +8,12 @@ st.title("Température malgré la clim")
 if "dfs" not in st.session_state:
     st.session_state["dfs"] = []
 
+# On chois le df en local avec un drag and drop
 uploaded_file = st.file_uploader("Choix de la base de données")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, sep=",", decimal=".")
-    
+
+    # On nettoye les données pour pouvoir les exploiter (format/doublons/on drop des colonnes)
     df["time"] = df["time"].str.split(".", n=1).str[0]
     df['time'] = df['time'].str.replace("T", " ")
     df.index = df.time
@@ -26,14 +28,17 @@ if uploaded_file is not None:
     df_combined = pd.concat(st.session_state["dfs"])
     df_combined.sort(key=lambda df: df.index[0])
 
+    # Création et affichage du graph + des ballons car c'est mignon
     fig = px.line(df_combined, x=df_combined.index, y="value")
     fig.update_layout(title="Graphique de la température dans l'open space")
     st.balloons()
     st.write(fig)
 
+    # Affichage des données
     st.write("Voici les données combinées de tous les fichiers :")
     st.write(df_combined)
 
+    # Possibilité de télécharger les données (dans les téléchargement) en sortie
     st.write("Vous pouvez télécharger les données.")
     st.write("Indiquez un nom et un cliquez sur le bouton 'Télécharger' ci-dessous.")
 
